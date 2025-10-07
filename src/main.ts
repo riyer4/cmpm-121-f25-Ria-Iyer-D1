870d9a3b6612b44eeb15583bba4c93288d8c57ea
riyer4 <riyer4@ucsc.edu>
2025-10-07 22:16:49 +0000

    added a counter (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,22 @@ import tb from "./tb.png";
 //   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 // `;
 
+let counter = 0;
+
 document.body.innerHTML = `
   <p>Get me more Taco Bell</p>
+  <p id="counter">${counter} tacos</p>
   <button id="TacoBellButton" style="background:none, pointer:none, cursor:none;">
   <img src="${tb}" class="icon" alt="Taco Bell Icon" />
   </button>
 `;
+
+const counterElement = document.getElementById("counter");
+
+document.getElementById("TacoBellButton")?.addEventListener("click", () => {
+  counter++;
+  console.log("Amount of Taco Bell: ", counter);
+  if (counterElement) {
+    counterElement.textContent = counter.toString();
+  }
+});
