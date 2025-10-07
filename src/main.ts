ecee93df6275ab30baa46e14d58214713ebaaafc
riyer4 <riyer4@ucsc.edu>
2025-10-07 22:04:16 +0000

    added a button (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+// import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
+import tb from "./tb.png";
+
+// document.body.innerHTML = `
+//   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+// `;
+
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <p>Get me more Taco Bell</p>
+  <button id="TacoBellButton" style="background:none, pointer:none, cursor:none;">
+  <img src="${tb}" class="icon" alt="Taco Bell Icon" />
+  </button>
 `;
+
+
+
+
+
